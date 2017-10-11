e811df4ec4887d95af4decbf0d07b187df7bee7a
Tapas Jena <tapas.friends@gmail.com>
2017-10-11 23:39:40 +0800

    Docker files added (#42)
    * Dependencies updated
    
    * jQuery plugin removed and color changed
    
    * jQuery removed from the project
    
    * Docker files added
    
    * package lock

--- src/app/static/landing/landing.component.ts
+++ src/app/static/landing/landing.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, ViewChild, ElementRef, HostListener } from '@angular/core';
 declare var Particles: any;
 declare var classie: any;
-declare var $: any;
 
 @Component({
   selector: 'tq-landing',
@@ -19,11 +18,9 @@ export class LandingComponent implements OnInit {
     minDistance: '100',
     connectParticles: 'true'
   };
-  @ViewChild('tabElem') el:ElementRef;
 
   header = document.querySelector('.navbar-default');
   didScroll = false;
-  changeHeaderOn = 50;
 
   constructor() { }
 
@@ -31,17 +28,9 @@ export class LandingComponent implements OnInit {
     Particles.init(this.particleOptions);
   }
 
-  ngAfterViewInit() {
-    // ABOUT SECTION TABS
-    $(this.el.nativeElement).easytabs({
-      animationSpeed: 'normal',
-      updateHash: false
-    });
-  }
-
   @HostListener('document:scroll', ['$event'])
   onPageScroll(ev: UIEvent) {
-    if ((window.pageYOffset || document.documentElement.scrollTop) >= this.changeHeaderOn) {
+    if ((window.pageYOffset || document.documentElement.scrollTop) >= 50) {
       this.didScroll = true;
     } else {
       this.didScroll = false;
